2248d4dca053d305e6df17d43d4f85a6abaa2bc7
isabelamedeiros <smedeirosisabela@gmail.com>
2024-06-24 13:44:15 -0300

    Primeiro commit

---
+++ src/main/kotlin/com/br/labs/LabsApplication.kt
@@ -0,0 +1,11 @@
+package com.br.labs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LabsApplication
+
+fun main(args: Array<String>) {
+	runApplication<LabsApplication>(*args)
+}
---
+++ src/test/kotlin/com/br/labs/LabsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.labs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LabsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
